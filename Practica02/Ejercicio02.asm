764e5bfa63d89206243caa377f29e6af4d302004
Azrielx86 <edgar1569@outlook.com>
2024-09-01 22:03:12 -0600

    Agregados comentarios de la practica 2

--- Practica02/Ejercicio02.asm
+++ Practica02/Ejercicio02.asm
@@ -1,29 +1,34 @@
 	PROCESSOR 16f877A
 	INCLUDE <p16f877A.inc> 
-AX	EQU 0x40
+AX	EQU 0x40            ; Registro para almacenar el valor menor encontrado
 	ORG 0
 	GOTO INICIO 
 	ORG 5
 INICIO:
-	BCF		STATUS,7
-	BCF 	STATUS,RP1 ; Para seleccionar el primer banco
-	BCF 	STATUS,RP0
-	MOVLW 	0X20
-	MOVWF 	FSR
-	MOVF 	INDF,W
-	MOVWF	AX
-	INCF 	FSR
+	BCF		STATUS,7    ; Se limpia el registro IRP
+	BCF 	STATUS,RP1  ; Para seleccionar el primer banco
+	BCF 	STATUS,RP0  ; Se colocan RP1/0 en 00
+	MOVLW 	0X20        ; Se mueve la direccion del primer elemento en W
+	MOVWF 	FSR         ; y se coloca en FSR para el
+                        ; direccionamiento indirecto
+	MOVF 	INDF,W      ; Se obtiene el valor del primer elemento
+                        ; del arreglo
+	MOVWF	AX          ; Se guarda en AX
+	INCF 	FSR         ; Incrementa en uno el apuntador
 LOOP:
-	MOVF	INDF,W
-	SUBWF	AX,W
-	
-	BTFSS	STATUS,0
-	GOTO 	MENOR
-	MOVF	INDF,W
-	MOVWF	AX
-MENOR:
-	INCF 	FSR
-	BTFSS 	FSR,6
-	GOTO 	LOOP
-	GOTO 	$
+	MOVF	INDF,W      ; Obtiene el valor del primer elemento y
+                        ; lo almacena en W
+	SUBWF	AX,W        ; Se compara con AX
+	BTFSS	STATUS,0    ; | C = STATUS[0]
+	GOTO 	MAYOR       ; Si el valor del carry es 1, significa que 
+                        ; el valor del arreglo obtenido es menor
+                        ; que el almacenado en AX
+	MOVF	INDF,W      ; Se mueve el nuevo valor encontrado en W  
+	MOVWF	AX          ; Y posteriormente en AX
+MAYOR:
+	INCF 	FSR         ; Incrementa en uno el apuntador
+	BTFSS 	FSR,6       ; Comprueba si llego al ultimo elemento del
+                        ; arreglo en 0x3F
+	GOTO 	LOOP        ; Si faltan elementos, continua el loop
+	GOTO 	$           ; Final del programa
 	END
--- Practica02/Ejercicio03.asm
+++ Practica02/Ejercicio03.asm
@@ -1,59 +1,53 @@
 	PROCESSOR 16f877A
 	INCLUDE <p16f877A.inc> 
-AX	EQU 0x40 ; Valor Menor
+AX	EQU 0x40 ; Valor Menor actual
 BX	EQU	0x41 ; Direccion del siguiente indice
-CX	EQU	0x42 ; Direccion del valor menor encontrado
+CX	EQU	0x42 ; Direccion del ultimo valor menor encontrado
 DX	EQU	0x43 ; Arreglo[BX]
 	ORG 0
 	GOTO INICIO 
 	ORG 5
 INICIO:
-	BCF		STATUS,7
-	BCF 	STATUS,RP1
+	BCF		STATUS,7    ; Se limpia el registro IRP
+	BCF 	STATUS,RP1  ; Se limpian los registros para el acceso indirecto
 	BCF 	STATUS,RP0
-	MOVLW 	0x20
-	MOVWF 	FSR
-	MOVWF	BX ; Guarda la dirección inicla en BX
-	MOVF 	INDF,W
-	MOVWF	AX
-	INCF 	FSR
-LOOP:
-	MOVF	INDF,W
-	SUBWF	AX,W
-	
-	BTFSS	STATUS,0
-	GOTO 	MAYOR
-	MOVF	INDF,W
-	MOVWF	AX
-	MOVF	FSR,W
-	MOVWF	CX
-MAYOR:
-	INCF 	FSR
-	BTFSS 	FSR,6
-	GOTO 	LOOP
-; Swap
-	MOVF	BX,W
-	MOVWF	FSR
-	MOVF	INDF,W
-	MOVWF	DX
-	MOVF	AX,W
-	MOVWF	INDF
-	
-	MOVF 	CX,W
-	MOVWF	FSR
-	MOVF	DX,W
-	MOVWF	INDF
-	
-; Incrementar BX para no volver a pasar por los valores ya ordenados
-	INCF	BX
-	MOVF	BX,W
-	MOVWF	FSR
-	
-	MOVF	INDF,W
-	MOVWF	AX
-	INCF	FSR
-	
-	BTFSS 	FSR,6
-	GOTO	LOOP
-	GOTO 	$
+	MOVLW 	0x20        ; Se inicializa el puntero FSR en el primer elemento
+	MOVWF 	FSR         ; de la lista, ubicado en 0x20 del banco 0
+	MOVWF	BX          ; y se copia la direccion tambien en BX
+	MOVF 	INDF,W      ; Se guarda en W el valor de la direccion a la que apunta FSR
+	MOVWF	AX          ; Crea una copia del valor almacenado en FSR tambien en AX
+	INCF 	FSR         ; Incrementa en uno el apuntador FSR
+LOOP:                   ; ==
+	MOVF	INDF,W      ; Guarda el valor en el que apunta FSR en W
+	SUBWF	AX,W        ; y lo compara con el valor guardado en AX
+	BTFSS	STATUS,0    ; Revisa si W < AX mediante el bit de carry
+	GOTO 	MAYOR       ; Si es menor, continua la ejecucion,
+                        ; en caso contrario pasa a la etiqueta MAYOR.
+	MOVF	INDF,W      ; Guarda una copia del valor en el registro W
+	MOVWF	AX          ; Guarda tambien una copia en AX
+	MOVF	FSR,W       ; Obtiene la direccion de memoria donde se encontro ese valor
+	MOVWF	CX          ; Guarda la direccion de memoria en CX
+MAYOR:                  ; == 
+	INCF 	FSR         ; Si es mayor, incrementa el puntero de FSR
+	BTFSS 	FSR,6       ; Revisa si ya recorrio los elementos hasta 0x3F
+	GOTO 	LOOP        ; Si faltan elementos por ordenar, repite el loop
+	MOVF	BX,W        ; == Swap: Se recupera la direccion del siguiente valor que se va a ordenar
+	MOVWF	FSR         ; y se mueve a FSR
+	MOVF	INDF,W      ; Se guarda una copia del elemento a ordenar en W
+	MOVWF	DX          ; Se guarda en el registro temporal DX
+	MOVF	AX,W        ; Se mueve el ultimo valor menor encontrado a W
+	MOVWF	INDF        ; Se guarda el valor en su indice correspondiente
+	MOVF 	CX,W        ; Se obtiene la direccion del valor que se intercambio
+	MOVWF	FSR         ; Se copia la direccion a FSR
+	MOVF	DX,W        ; Se coloca el valor intercambiado en donde se encontraba el ultimo valor menor
+	MOVWF	INDF        ; Incrementa en uno el puntero
+	INCF	BX          ; Incrementar BX para no volver a pasar por los valores ya ordenados
+	MOVF	BX,W        ; Se copia el valor de BX al
+	MOVWF	FSR         ; puntero FSR
+	MOVF	INDF,W      ; Obtiene el valor del primer elemento del array
+	MOVWF	AX          ; Se guarda el valor en AX
+	INCF	FSR         ; Se incrementa en uno el puntero
+	BTFSS 	FSR,6       ; Comprueba si ya era el ultimo elemento del array (en 0x3F)
+	GOTO	LOOP        ; Si faltan elementos, regresa a Loop
+	GOTO 	$           ; Cuando se ordena toda la lista, se queda la ejecucion en esta instruccion
 	END
