04fef7fa254d3a3d77972391d6bbe5f53f1d5260
Rodrigo Tapia <rodrigotana15@gmail.com>
2024-10-22 20:38:08 -0600

    Agregados comentarios adicionales a ejercicio01.asm

--- Practica07/Ejercicio01.asm
+++ Practica07/Ejercicio01.asm
@@ -4,21 +4,12 @@
 	ORG 0
 	GOTO INICIO
 	ORG 5
+
 INICIO:
-    BSF     STATUS,RP0
-    BCF     STATUS,RP1
-    
-    BSF     TXSTA,BRGH  ; 
-    MOVLW   0x81        ; Establece el baudrate en 9600 baudios.
-    MOVWF   SPBRG
-    BCF     TXSTA,SYNC  ; Configura el modo asincrono
-    BSF     TXSTA,TXEN  ; Habilita la transmision
-    
-    BCF     STATUS,RP0
-    
-    BSF     RCSTA,SPEN  ; Habilita el puerto serie
-    BSF     RCSTA,CREN  ; Habilita la recepcion de datos.
-    
+
+    CALL    CONFIG_INICIAL
+
+;Espera a recibir un dato a través del puerto serie
 RECIBE:
     BTFSS   PIR1,RCIF   ; Comprueba si ya se recibieron datos
     GOTO    RECIBE
@@ -27,10 +18,31 @@ RECIBE:
     MOVWF   TXREG       ; Mueve el dato obtenido a W
     
     BSF     STATUS,RP0  ; Cambio del banco para el registro de transmision
+
 TRANSMITE:
     BTFSS   TXSTA,TRMT  ; Comprueba si ya se transmitio el dato
     GOTO    TRANSMITE
     BCF     STATUS,RP0  ; Regresa al banco 0
     GOTO    RECIBE
+
+CONFIG_INICIAL:
+    ;Cambia al banco 1
+    BSF     STATUS,RP0
+    BCF     STATUS,RP1
+    
+    BSF     TXSTA,BRGH  ; Configura la bandera BRGH para alta velocidad
+
+    ;Establece el baudrate en 9600 baudios.
+    MOVLW   0x81        
+    MOVWF   SPBRG
+
+    BCF     TXSTA,SYNC  ; Configura el modo asincrono
+    BSF     TXSTA,TXEN  ; Habilita la transmision
+    
+    ;Cambia al banco 0
+    BCF     STATUS,RP0
+    
+    BSF     RCSTA,SPEN  ; Habilita el puerto serie
+    BSF     RCSTA,CREN  ; Habilita la recepcion de datos.
     
     END
