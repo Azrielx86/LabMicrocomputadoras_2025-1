35dc3453245bb645df17916d3abc9ff6bda1b729
Rodrigo Tapia <rodrigotana15@gmail.com>
2024-10-22 22:01:37 -0600

    Modificaciones Ejercicio02.asm
    - Se sustituyó el código original por el del ejercicio01.asm, se agregaron las instrucciones necesarias para la salida de los datos al puerto B
    -Se agregaron comentarios faltantes.

--- Practica07/Ejercicio02.asm
+++ Practica07/Ejercicio02.asm
@@ -1,37 +1,55 @@
-	processor 16f877
-	include<p16f877.inc>
-
+	PROCESSOR 16F877A
+	INCLUDE<p16F877a.inc>
+	
 	ORG 0
-	GOTO inicio
+	GOTO INICIO
 	ORG 5
-inicio
-	BSF STATUS,RP0
-	BCF STATUS,RP1
-	CLRF	TRISB
-	BSF TXSTA,BRGH
-	MOVLW D'129'
-	MOVWF SPBRG
 
-	BCF TXSTA,SYNC
-	BSF TXSTA,TXEN
-	BCF STATUS,RP0
-	BSF RCSTA,SPEN
-	BSF RCSTA,CREN
-	CLRF PORTB
+INICIO:
 
+    CALL    CONFIG_INICIAL
+
+;Espera a recibir un dato a través del puerto serie
 RECIBE:
-	BTFSS PIR1,RCIF
-	GOTO RECIBE
+    BTFSS   PIR1,RCIF   ; Comprueba si ya se recibieron datos
+    GOTO    RECIBE
+    
+    MOVFW   RCREG       ; Almacena el dato leido en W
+    MOVWF   TXREG       ; Mueve el dato obtenido a W
+    MOVWF	PORTB
 
-	MOVF RCREG,W
-	MOVWF TXREG
-	MOVWF	PORTB
-	BSF STATUS,RP0
+    BSF     STATUS,RP0  ; Cambio del banco para el registro de transmision
 
-TRASMITE:
-	BTFSS TXSTA,TRMT
-	GOTO TRASMITE
-	BCF STATUS,RP0
-	GOTO RECIBE
+TRANSMITE:
+    BTFSS   TXSTA,TRMT  ; Comprueba si ya se transmitio el dato
+    GOTO    TRANSMITE
+    BCF     STATUS,RP0  ; Regresa al banco 0
+    GOTO    RECIBE
 
-END
+CONFIG_INICIAL:
+    ;Cambia al banco 1
+    BSF     STATUS,RP0
+    BCF     STATUS,RP1
+
+	CLRF 	TRISB		;Puerto B como salida
+    
+    BSF     TXSTA,BRGH  ; Configura la bandera BRGH para alta velocidad
+
+    ;Establece el baudrate en 9600 baudios.
+    MOVLW   0x81        
+    MOVWF   SPBRG
+
+    BCF     TXSTA,SYNC  ; Configura el modo asincrono
+    BSF     TXSTA,TXEN  ; Habilita la transmision
+    
+    ;Cambia al banco 0
+    BCF     STATUS,RP0
+    
+    BSF     RCSTA,SPEN  ; Habilita el puerto serie
+    BSF     RCSTA,CREN  ; Habilita la recepcion de datos.
+
+	CLRF PORTB
+    
+    RETURN
+
+    END
